7700516e9a39c224c145658612859d4d5d6cb160
Yesid.dev <yesid.yag@gmail.com>
2019-04-30 23:16:35 +0000

    [Fix] last number

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,4 @@ for c in range(ord('z'), ord('a') - 1, -1):
     if(c % 2 == 0):
         print("{}".format(chr(c)), end="")
     else:
-        print("{}".format(chr(c - 32)), end ="")
+        print("{}".format(chr(c-32)), end="")
