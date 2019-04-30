efa2bdc0b90a2fd9ba51917474d11fc45b1811c0
Yesid.dev <yesid.yag@gmail.com>
2019-04-30 15:40:55 +0000

    [Feature] print the alphabet

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,10 +6,10 @@ if number < 0:
     lastDigit = lastDigit * -1
 if lastDigit > 5:
     print("Last digit of {:d} is {:d} and is greater than 5"
-          .format(number,lastDigit))
+          .format(number, lastDigit))
 elif lastDigit == 0:
     print("Last digit of {:d} is {:d} and is 0"
-          .format(number,lastDigit))
+          .format(number, lastDigit))
 else:
     print("Last digit of {:d} is {:d} and is less than 6 and not 0"
-          .format(number,lastDigit))
+          .format(number, lastDigit))
