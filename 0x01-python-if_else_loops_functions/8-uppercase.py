c210fae0b9b1e6efe3004717e2810d5c1f08a236
Yesid.dev <yesid.yag@gmail.com>
2019-04-30 19:52:23 +0000

    [Feature] prints a string in uppercase followed by a new line.

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        print("{}".format(chr(ord(str[i]) - 32)
+                          if ord(str[i]) >= ord('a') and
+                          ord(str[i]) <= ord('z')
+                          else str[i]), end="")
+    print("")
