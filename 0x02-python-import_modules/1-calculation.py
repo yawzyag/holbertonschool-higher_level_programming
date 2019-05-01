d08449a560fdf2de66b5eb839bc21a7ddfb7ef67
yawzyag <yesid.yag@gmail.com>
2019-05-01 12:47:42 -0500

    [Feature] prints the number of and the list of its arguments.

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-a = 10
-b = 5
 if __name__ == "__main__":
     from calculator_1 import *
+    a = 10
+    b = 5
 
     print("{:d} + {:d} = {:d}".format(a, b, add(10, 5)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(10, 5)))
