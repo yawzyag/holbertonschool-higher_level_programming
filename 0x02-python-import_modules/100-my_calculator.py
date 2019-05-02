4becfd1d1d6e9104a1c3cae6dfd816e92379c9f3
yawzyag <yesid.yag@gmail.com>
2019-05-01 19:09:01 -0500

    [Feature] Calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1 as cal
+    import sys
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        symbol = int(ord(sys.argv[2]))
+
+        if symbol == ord('+'):
+            print("{:d} + {:d} = {:d}".format(a, b, a + b))
+        elif symbol == ord("-"):
+            print("{:d} - {:d} = {:d}".format(a, b, a - b))
+        elif symbol == ord("*"):
+            print("{:d} * {:d} = {:d}".format(a, b, a * b))
+        elif symbol == ord("/"):
+            print("{:d} / {:d} = {:f}".format(a, b, a / b))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
