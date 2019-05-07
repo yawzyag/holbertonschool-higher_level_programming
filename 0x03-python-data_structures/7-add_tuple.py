482be87aa4e88d05acd042dfbdcdac76342900a7
Yesid.dev <yesid.yag@gmail.com>
2019-05-07 21:58:24 +0000

    [Fix] help

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) is 2 and len(tuple_b) is 2:
-        t = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-    elif len(tuple_a) is 0 and len(tuple_b) is 0:
-        t = 0, 0
-    elif len(tuple_a) is 1 and len(tuple_b) is 1:
-        t = tuple_a[0] + tuple_b[0], 0
-    elif len(tuple_b) is 0 and len(tuple_a) is 2:
-        return tuple_a
-    elif len(tuple_a) is 0 and len(tuple_b) is 2:
-        return tuple_b
-    elif len(tuple_b) is 1 and len(tuple_a) is 2:
-        t = tuple_a[0] + tuple_b[0], tuple_a[1] + 0
-    elif len(tuple_a) is 1 and len(tuple_b) is 2:
-        t = tuple_a[0] + tuple_b[0], tuple_b[1] + 0
-    return t
+    t = (0, 0)
+    if tuple_a == ():
+        tuple_a = t
+    if tuple_b == ():
+        tuple_b = t
+    if len(tuple_a) == 1:
+        tuple_a = tuple_a[0] + 0, 0
+    if len(tuple_b) == 1:
+        tuple_b = tuple_b[0] + 0, 0
+    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
+        return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
