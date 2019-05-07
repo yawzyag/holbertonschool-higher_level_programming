3b9ae261a08a46f55e547c93c7a106d6269a83e8
Yesid.dev <yesid.yag@gmail.com>
2019-05-07 22:27:19 +0000

    [Fix] none

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
+    if my_list == []:
         return None
-    elif my_list is not None:
+    else:
         num = 0
         for i in my_list:
             if i > num:
