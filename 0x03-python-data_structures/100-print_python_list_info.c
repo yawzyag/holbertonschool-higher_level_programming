92d2bf2b644b9bc9be33eb0866d596c1f6c71e83
Yesid.dev <yesid.yag@gmail.com>
2019-05-08 01:12:49 +0000

    [Feature] none

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,12 @@
+#include <listobject.h>
+#include <object.h>
+/**
+ * print_python_list_info - print all nodes
+ * @p: node
+ *
+ * Return: number of nodes
+ */
+void print_python_list_info(PyObject *p)
+{
+	(void) p
+}
