1c2ace9d335a2bc5a6adde39514b21dc46fc0c1a
Yesid.dev <yesid.yag@gmail.com>
2019-05-09 21:42:08 +0000

    [Feature] multiply

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key, value in sorted(a_dictionary.items()):
+    b_dictionary = a_dictionary.copy()
+    for key, value in sorted(b_dictionary.items()):
         d1 = {key: value * 2}
-        a_dictionary.update(d1)
-    return a_dictionary
+        b_dictionary.update(d1)
+    return b_dictionary
