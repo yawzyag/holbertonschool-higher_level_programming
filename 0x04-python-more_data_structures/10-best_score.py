a101deb23a07bd60ab226304535ae051a05a812e
Yesid.dev <yesid.yag@gmail.com>
2019-05-09 22:00:43 +0000

    [Fix] check for empty dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
-        sorted_best = sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True)
+    if a_dictionary is not None and len(a_dictionary) is not 0:
+        sorted_best = sorted(a_dictionary.items(),
+                             key=lambda x: x[1], reverse=True)
         best = list(sorted_best[0])[0]
         return best
