c8171e956c98f65bbd038459e426ff3ca8922b07
Yesid.dev <yesid.yag@gmail.com>
2019-05-10 00:03:11 +0000

    [Fix] print

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list == []:
-        return 0
-    num1 = 0
-    num2 = 0
-    for tuple_1 in my_list:
-        num1 = num1 + (tuple_1[0] * tuple_1[1])
-        print(num1)
-        num2 = num2 + tuple_1[1]
-    num1 = num1 / num2
-    return num1
+    if my_list is not []:
+        num1 = 0
+        num2 = 0
+        for tuple_1 in my_list:
+            num1 = num1 + (tuple_1[0] * tuple_1[1])
+            num2 = num2 + tuple_1[1]
+        num1 = num1 / num2
+        return num1
+    return 0
