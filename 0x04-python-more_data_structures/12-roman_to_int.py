654c14c55a876276a85e62e33f99bac785afae80
Yesid.dev <yesid.yag@gmail.com>
2019-05-10 01:53:56 +0000

    [Fix] pepa

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,12 @@
 def roman_to_int(roman_string):
     if type(roman_string) is not str:
         return 0
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    ro = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
     for i in range(len(roman_string)):
-            value = roman[roman_string[i]]
-            if i + 1 < len(roman_string) and roman[roman_string[i + 1]] > value:
-                num -= value
+            val = ro[roman_string[i]]
+            if (i + 1 < len(roman_string) and ro[roman_string[i + 1]] > val):
+                num -= val
             else:
-                num += value
+                num += val
     return num
