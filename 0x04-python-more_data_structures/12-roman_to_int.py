0b0b9be3e9c8e5db0d907367e34c453345ce88e0
Yesid.dev <yesid.yag@gmail.com>
2019-05-10 01:48:44 +0000

    [Fix] range and loop

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) is not str:
+        return 0
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
     for i in range(len(roman_string)):
-        try:
             value = roman[roman_string[i]]
-            # If the next place holds a larger number, this value is negative
-            if i+1 < len(roman_string) and roman[roman_string[i+1]] > value:
+            if i + 1 < len(roman_string) and roman[roman_string[i + 1]] > value:
                 num -= value
-            else: num += value
-        except:
-            return None
+            else:
+                num += value
     return num
