a337af48dc6de8cb21681c329065c1c3f9f42f53
Yesid.dev <yesid.yag@gmail.com>
2019-05-20 18:02:18 +0000

    [Fix] 0 list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    num = 0
     new_list = []
     for i in range(list_length):
+        num = 0
         try:
             num = my_list_1[i] / my_list_2[i]
         except IndexError:
