40f752e4e22e0431555e63402382d9213b87f1a5
Yesid.dev <yesid.yag@gmail.com>
2019-05-20 17:59:40 +0000

    [Feature] div two list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    num = 0
+    new_list = []
+    for i in range(list_length):
+        try:
+            num = my_list_1[i] / my_list_2[i]
+        except IndexError:
+            print("out of range")
+        except (TypeError, ValueError):
+            print("wrong type")
+        except ZeroDivisionError:
+            num = 0
+            print("division by 0")
+        finally:
+            new_list.append(num)
+    return new_list
