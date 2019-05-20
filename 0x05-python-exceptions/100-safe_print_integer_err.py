3da7fee6e6d1d3e1450c32f93b7734b35b36c4bc
Yesid.dev <yesid.yag@gmail.com>
2019-05-20 20:11:56 +0000

    [Feature] stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+# from __future__ import print_function
+import sys
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except (TypeError, ValueError) as holi:
-        print("Exception: {}".format(holi))
+        print("Exception: {}".format(holi), file=sys.stderr)
         return False
