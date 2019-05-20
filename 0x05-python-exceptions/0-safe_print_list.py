c73f4f7fdfcff042d4f33132cdfb638c43d6ea90
Yesid.dev <yesid.yag@gmail.com>
2019-05-20 16:21:11 +0000

    [Fix] index error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for num in range(x):
         try:
             print("{}".format(my_list[num]), end="")
-        except (ValueError, IndexError):
+        except IndexError:
             print("")
             return num
     print("")
