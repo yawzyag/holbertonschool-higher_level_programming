e2d7f3dc06b57b9b53b4b0f1a14387a3b0471001
Yesid.dev <yesid.yag@gmail.com>
2019-05-20 16:19:51 +0000

    [Feature] print safe list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    num = 0
+    for num in range(x):
+        try:
+            print("{}".format(my_list[num]), end="")
+        except (ValueError, IndexError):
+            print("")
+            return num
+    print("")
+    num += 1
+    return num
