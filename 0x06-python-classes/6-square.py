88959f895f62a091317d2cfe4ded0d2f97e474f2
Yesid.dev <yesid.yag@gmail.com>
2019-05-21 22:05:51 +0000

    [Fix] order of if

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,8 +47,8 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (type(value[0]) is not int or type(value[1]) is not int or
-                len(value) is not 2 or type(value) is not tuple or
+        if (type(value) is not tuple or len(value) is not 2 or
+            type(value[0]) is not int or type(value[1]) is not int or
                 value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
