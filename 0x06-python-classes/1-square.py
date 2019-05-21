6a39f1b9eb9a85a2a3f5f6631379581c8b3d50e6
Yesid.dev <yesid.yag@gmail.com>
2019-05-21 19:49:36 +0000

    [Feature] square area

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
 class Square:
-    """ This is a Square class
+    """ This is a Square class """
 
-    Attributes:
-        __size (int): Size of the square.
-
-    """
-    __size = 0
-
-    def __init__(self, __size):
+    def __init__(self, size):
         """Example of docstring on the __init__ method.
         Args:
-            __size (int): Size of the square obj.
+            size (int): Size of the square obj.
         """
-        self.__size = __size
+        self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -13,3 +13,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
+    def area(self):
+        """ area of square """
+        return self.__size * self.__size
