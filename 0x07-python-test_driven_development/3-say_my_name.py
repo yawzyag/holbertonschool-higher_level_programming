babcb3242f92112c96543405941a835dea61866f
Yesid.dev <yesid.yag@gmail.com>
2019-05-23 23:05:08 +0000

    [Feature] function that prints My name is <first name> <last name> and test case

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" This is necesary ?
+    I
+    dont
+    know
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """function to add two numbers.
+       I dont know
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    elif  type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
