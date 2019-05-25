f2f95a1c9e843951e6de6b8f8b733f1411316951
Yesid.dev <yesid.yag@gmail.com>
2019-05-25 00:26:57 +0000

    [Feature]  function that multiplies 2 matrices by using the module NumPy

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""multipli
+    whit numpy
+
+"""
+import numpy as np
+
+def lazy_matrix_mul(m_a, m_b):
+    """multiplin whit numpy
+    args
+       m_a
+       m_b
+    """
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
+    """
+    if type(m_a) is not list:
+        raise TypeError("m_a must be a list")
+    if type(m_b) is not list:
+        raise TypeError("m_b must be a list")
+    if not any(isinstance(el, list) for el in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not any(isinstance(el, list) for el in m_b):
+        raise TypeError("m_b must be a list of lists")
+    if any(len(list(el)) is 0 for el in m_a or len(m_a) is 0):
+       raise TypeError("m_a can't be empty")
+    if any(len(list(el)) is 0 for el in m_b or len(m_b) is 0):
+       raise TypeError("m_b can't be empty")
+    list1 = []
+    err1 = "each row of m_b must should be of the same size"
+    err2 = "m_a should contain only integers or floats"
+    err3 = "m_b should contain only integers or floats"
+    row_lena = len(m_a[0])
+    row_lenb = len(m_b[0])
+    if row_lena is not len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+    for i in range(0,len(m_a)):
+        if row_lena is not len(m_a[i]):
+            raise TypeError("each row of m_a must should be of the same size")
+        temp=[]
+        for j in range(0,len(m_b[0])):
+            if row_lenb is not len(m_b[j]):
+                raise TypeError(err1)
+            for k in range(0,len(m_a[0])):
+                if type(m_a[i][k]) is not int and type(m_a[i][k]) is not float:
+                    raise TypeError(err2)
+                if type(m_b[k][j]) is not int and type(m_b[k][j]) is not float:
+                    raise TypeError(err3)
+    return np.dot(m_a, m_b)
