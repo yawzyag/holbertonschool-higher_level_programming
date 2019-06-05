5922024acee415c223baccde02d822a487c3183c
yawzyag <yesid.yag@gmail.com>
2019-06-05 16:23:22 +0000

    [Feature] Write a function that reads a text file (UTF8) and prints it to stdout:

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,4 +3,5 @@
 
 def read_file(filename=""):
     with open(filename, encoding="UTF") as juanito:
-        print("{}".format(juanito.read()), end="")
+        for line in juanito:
+            print("{}".format(line), end="")
