7d4ef1c07f1bb4ebe066c36a3a98523410adf104
yawzyag <yesid.yag@gmail.com>
2019-06-04 21:09:50 -0500

    [Fix] pep

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,5 +6,4 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     def __init__(self, size):
-        self.__size = size
-        Rectangle.__init__(self, self.__size, self.__size)
+        super().__init__(size, size)
