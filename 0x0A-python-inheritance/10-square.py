90b6a4e408d45abe25c9bce627cd8c20a4aea2bc
yawzyag <yesid.yag@gmail.com>
2019-06-06 01:26:37 +0000

    [Feature] cosito

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,4 +6,5 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     def __init__(self, size):
+        super().integer_validator("size", size)
         super().__init__(size, size)
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -6,6 +6,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     def __init__(self, size):
+        super().integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
 
