63a7b18de9429ec11ea84c6c00589073951a2e6d
Yesid.dev <yesid.yag@gmail.com>
2019-06-11 17:45:09 +0000

    [Fix] comments

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
+""" testing comments """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """ testing comments """
+
     def __init__(self, size, x=0, y=0, id=None):
+        """ testing comments """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """ testing comments """
         return ("[Square] ({}) {}/{} - {}"
                 .format(self.id, self.x, self.y, self.size))
 
     def update(self, *args, **kwargs):
+        """ testing comments """
         listm = ["id", "size", "x", "y"]
 
         if len(args) != 0:
@@ -21,14 +27,17 @@ class Square(Rectangle):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """ testing comments """
         return {'x': self.x, 'y': self.y, 'id': self.id,
                 'size': self.height}
 
     @property
     def size(self):
+        """ testing comments """
         return super().width
 
     @size.setter
     def size(self, value):
+        """ testing comments """
         super(Square, self.__class__).width.fset(self, value)
         super(Square, self.__class__).height.fset(self, value)
