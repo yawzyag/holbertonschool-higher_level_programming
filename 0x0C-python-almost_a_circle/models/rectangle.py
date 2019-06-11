910ef52c6b99d2e5a9b5f75caa5dd1b870368f32
Yesid.dev <yesid.yag@gmail.com>
2019-06-11 17:36:09 +0000

    [Fix] comments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-
+""" testing comments """
 from models.base import Base
 
 
 class Rectangle(Base):
+    """ testing comments """
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ testing comments """
         super().__init__(id)
         self.width = width
         self.height = height
