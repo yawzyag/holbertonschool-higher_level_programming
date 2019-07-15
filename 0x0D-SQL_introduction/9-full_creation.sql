79dcaf3dd54af8d6e64f11c93583f5938bb4d572
yawzyag <yesid.yag@gmail.com>
2019-07-15 18:01:12 +0000

    [feature] adding 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table VALUES(1, "John", 10);
 INSERT INTO second_table VALUES(2, "Alex", 3);
 INSERT INTO second_table VALUES(3, "Bob", 14);
