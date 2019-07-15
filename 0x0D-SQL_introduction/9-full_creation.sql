3a8875a13755e3feca991e44e3fa93a2b0ba9054
yawzyag <yesid.yag@gmail.com>
2019-07-15 17:59:06 +0000

    [Feature] adding files

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUES(1, "John", 10);
+INSERT INTO second_table VALUES(2, "Alex", 3);
+INSERT INTO second_table VALUES(3, "Bob", 14);
+INSERT INTO second_table VALUES(4, "George", 8);
