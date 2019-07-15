3de08f4d80795a172069b5fcbfbc67775a90845b
yawzyag <yesid.yag@gmail.com>
2019-07-15 22:30:45 +0000

    [Feature] adding 103

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-SELECT state, MAX(value) as max_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY state LIMIT 3;
+SELECT state, MAX(value) as max_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY state ORDER BY state LIMIT 3;
