7ccbfa39f70662c6ad65e394f443fbe4db9949d9
yawzyag <yesid.yag@gmail.com>
2019-07-15 22:24:28 +0000

    [Feature] adding 102

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) as avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
