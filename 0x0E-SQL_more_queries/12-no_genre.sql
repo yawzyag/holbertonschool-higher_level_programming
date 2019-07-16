58d5783c106a9a70947a627d5447e9ffc982bdce
yawzyag <yesid.yag@gmail.com>
2019-07-16 20:43:58 +0000

    [Fix] problem

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
-  FROM tv_show_genres
-  RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+  FROM tv_shows
+  LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
   WHERE tv_show_genres.genre_id IS NULL
   GROUP BY tv_shows.title, tv_show_genres.genre_id;
