c72e3ec344854f91b7fcb14cdb9cc8c0dce137db
yawzyag <yesid.yag@gmail.com>
2019-07-16 22:08:42 +0000

    [Fix] adding 16 group

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -6,4 +6,4 @@ LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
 ON tv_show_genres.genre_id  = tv_genres.id 
-ORDER BY tv_shows.title ASC;
+GROUP BY tv_shows.title, tv_genres.name;
