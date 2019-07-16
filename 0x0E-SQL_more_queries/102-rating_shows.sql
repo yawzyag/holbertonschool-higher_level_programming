4c38dfe048b0c59518d684fa5c7abd4663938f91
yawzyag <yesid.yag@gmail.com>
2019-07-16 23:05:36 +0000

    [Feature] adding tomatoes

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+  FROM tv_shows
+  JOIN tv_show_ratings
+  ON tv_shows.id = tv_show_ratings.show_id
+  GROUP BY tv_show_ratings.show_id
+  ORDER BY rating DESC;
