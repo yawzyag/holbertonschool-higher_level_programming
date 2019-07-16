e6d1ee7ba60788372268abb51bbcd27a8dc2b237
yawzyag <yesid.yag@gmail.com>
2019-07-16 21:37:34 +0000

    [Feature] adding 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+SELECT tv_genres.name
+  FROM tv_genres
+  JOIN tv_show_genres
+  ON tv_genres.id = tv_show_genres.genre_id
+  JOIN tv_shows
+  ON tv_shows.id = tv_show_genres.show_id
+  WHERE tv_shows.title = "Dexter"
+
