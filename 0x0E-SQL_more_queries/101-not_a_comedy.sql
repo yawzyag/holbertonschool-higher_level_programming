affca370c4782a75dd02270c8b071bcac1b604ec
yawzyag <yesid.yag@gmail.com>
2019-07-16 22:46:42 +0000

    [Feature] holi

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,13 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.title NOT IN (
+  SELECT tv_shows.title
+  FROM tv_shows
+  JOIN tv_show_genres
+  ON tv_shows.id = tv_show_genres.show_id
+  JOIN tv_genres
+  ON tv_genres.id = tv_show_genres.genre_id
+  WHERE tv_genres.name = "Comedy")
+  ORDER BY tv_shows.title ASC;
