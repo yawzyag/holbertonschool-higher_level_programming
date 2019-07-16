5a8b11ef8045ccfb154d601e4b711dfa8f06c64f
yawzyag <yesid.yag@gmail.com>
2019-07-16 20:46:18 +0000

    [Fix]

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++
@@ -1,5 +0,0 @@
-SELECT tv_shows.title, tv_show_genres.genre_id
-  FROM tv_shows
-  LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-  WHERE tv_show_genres.genre_id IS NULL
-  GROUP BY tv_shows.title, tv_show_genres.genre_id;
