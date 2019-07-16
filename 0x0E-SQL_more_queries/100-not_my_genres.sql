70d54c8e93f24c138b0517359fdfeb7dc9e488cc
yawzyag <yesid.yag@gmail.com>
2019-07-16 22:41:02 +0000

    [Feature] lol

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -9,5 +9,5 @@ WHERE tv_genres.name NOT IN (
   ON tv_genres.id = tv_show_genres.genre_id
   JOIN tv_shows
   ON tv_shows.id = tv_show_genres.show_id
-  WHERE tv_shows.title = "Dexter"
-  ORDER BY tv_genres.name ASC);
+  WHERE tv_shows.title = "Dexter")
+  ORDER BY tv_genres.name ASC;
