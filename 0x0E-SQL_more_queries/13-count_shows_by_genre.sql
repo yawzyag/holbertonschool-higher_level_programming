21d285c5a92cb953feaff518fb692bc4162d4b39
yawzyag <yesid.yag@gmail.com>
2019-07-17 00:17:23 +0000

    [Feature] adding 1

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+  FROM tv_genres
+  JOIN tv_show_genres
+  ON tv_genres.id = tv_show_genres.genre_id
+  GROUP BY tv_show_genres.genre_id
+  ORDER BY number_of_shows DESC;
