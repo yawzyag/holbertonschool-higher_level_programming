4346a0125ed51b10eaca08af0e59fc784732a4fa
yawzyag <yesid.yag@gmail.com>
2019-08-09 23:18:17 +0000

    [Fix] pep8

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     serv = MySQLdb.connect(
         host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
     c = serv.cursor()
-    text_exec = "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC ".format(
-        argv[4])
+    text_exec = "SELECT * FROM states WHERE name LIKE\
+    '{}' ORDER BY id ASC ".format(argv[4])
     c.execute(text_exec)
     response = c.fetchall()
     for row in response:
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
         host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
     c = serv.cursor()
     c.execute("SELECT cities.name\
-              FROM cities\
-             WHERE cities.state_id = \
-            (SELECT states.id FROM states WHERE states.name = %s) ORDER BY cities.id ASC;", (argv[4],))
+              FROM cities WHERE cities.state_id = \
+              (SELECT states.id FROM states WHERE \
+              states.name = %s) ORDER BY cities.id ASC;", (argv[4],))
     response = c.fetchall()
     strings = []
     for item in response:
