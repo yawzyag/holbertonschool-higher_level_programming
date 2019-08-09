ec89b070bf7a724d3b6eb584df650f6c9ebe79fc
yawzyag <yesid.yag@gmail.com>
2019-08-09 22:47:42 +0000

    [Fix] pep 8

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" adding comentario """
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    serv = MySQLdb.connect(
+        host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    c = serv.cursor()
+    c.execute("SELECT cities.id, cities.name, states.name\
+              FROM cities JOIN states ON cities.state_id=states.id\
+              ORDER BY cities.id ASC")
+    response = c.fetchall()
+    for row in response:
+        print("{}".format(row))
+    c.close()
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" adding comentario """
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    serv = MySQLdb.connect(
+        host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    c = serv.cursor()
+    c.execute("SELECT cities.name\
+              FROM cities\
+             WHERE cities.state_id = \
+            (SELECT states.id FROM states WHERE states.name = %s) ORDER BY cities.id ASC;", (argv[4],))
+    response = c.fetchall()
+    strings = []
+    for item in response:
+        strings += item
+    print(", ".join(strings))
+    c.close()
