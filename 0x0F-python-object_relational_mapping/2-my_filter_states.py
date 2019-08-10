cdfdf0efe751cd5fd869f260490fc1c8ff6fc8ba
yawzyag <yesid.yag@gmail.com>
2019-08-10 19:08:57 +0000

    [fix] latin

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,12 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    serv = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    serv = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306)
     c = serv.cursor()
     text = "SELECT * FROM states WHERE name LIKE '{}'\
     COLLATE latin1_general_cs\
