eba5ddc8e55f7381832a6f185cff748c2ca46f2b
yawzyag <yesid.yag@gmail.com>
2019-08-10 20:23:43 +0000

    [Feature] adding to a database

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Start link class to table in database"""
+
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import (create_engine)
+    from model_state import Base, State
+    from sqlalchemy import desc, asc
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+        pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new = State(name="Louisiana")
+
+    session.add(new)
+    session.commit()
+    print(new.id)
+    session.close()
