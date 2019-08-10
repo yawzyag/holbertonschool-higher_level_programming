7aef7e38c423a50c098af52e040cbd07b625ab43
yawzyag <yesid.yag@gmail.com>
2019-08-10 19:17:30 +0000

    [Feature] adding files

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Start link class to table in database"""
+
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import (create_engine)
+    from model_state import Base, State
+    from sqlalchemy import desc, asc
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+        pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).filter(State.name == sys.argv[4]).all()
+    #print(type(result))
+    if len(result) != 0:
+        for row in result:
+            print("{}".format(row.id))
+    else:
+        print("Not found")
+    session.close()
