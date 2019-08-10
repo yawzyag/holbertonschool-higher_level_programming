a81734878252fd546f0f28ceb981d9daf6fb0219
yawzyag <yesid.yag@gmail.com>
2019-08-10 22:22:27 +0000

    [Feature] deleting files

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Start link class to table in database"""
+
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import (create_engine)
+    from model_state import Base, State
+    from sqlalchemy import desc, asc
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+        pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).filter(State.name.like("%a%"))
+
+    for r in result:
+        print(r.id)
+        session.delete(r)
+    session.commit()
+    session.close()
