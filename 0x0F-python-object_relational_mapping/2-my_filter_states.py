23d77887fd332fc7e46de619f96c99933c9b4f46
yawzyag <yesid.yag@gmail.com>
2019-08-10 18:51:25 +0000

    [Fix] aadinf file

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,12 +6,11 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    serv = MySQLdb.connect(
-        host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    serv = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = serv.cursor()
-    text_exec = "SELECT * FROM states WHERE name LIKE\
+    text = "SELECT * FROM states WHERE name LIKE\
     '{}' ORDER BY id ASC ".format(argv[4])
-    c.execute(text_exec)
+    c.execute(text)
     response = c.fetchall()
     for row in response:
         print(row)
