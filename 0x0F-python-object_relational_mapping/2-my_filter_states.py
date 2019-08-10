df304e5eae7510fa21ddbcda6b270a2e70cab4d1
yawzyag <yesid.yag@gmail.com>
2019-08-10 19:04:20 +0000

    [fix] collate

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,10 +6,11 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    serv = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    serv = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
     c = serv.cursor()
-    text = "SELECT * FROM states WHERE name LIKE\
-    '{}' ORDER BY id ASC ".format(argv[4])
+    text = "SELECT * FROM states WHERE name LIKE '{}'\
+    COLLATE latin1_general_cs\
+    ORDER BY id ASC ".format(argv[4])
     c.execute(text)
     response = c.fetchall()
     for row in response:
