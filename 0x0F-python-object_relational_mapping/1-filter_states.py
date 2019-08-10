0c29b4f4ebc4ff400cc80cf54e8911751066e3c0
yawzyag <yesid.yag@gmail.com>
2019-08-10 19:15:33 +0000

    [Fix] latin

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     serv = MySQLdb.connect(
         host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
     c = serv.cursor()
-    c.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC ")
+    c.execute("SELECT * FROM states WHERE name LIKE 'N%'\
+    COLLATE latin1_general_cs ORDER BY id ASC ")
     response = c.fetchall()
     for row in response:
         print(row)
