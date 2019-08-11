a199e11b26b430fe096d0d5a97e59f2203688919
yawzyag <yesid.yag@gmail.com>
2019-08-11 18:34:54 +0000

    [Feature] adding tab results

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    result = session.query(State).all()
+    result = session.query(State).order_by(State.id).all()
     # now State have the conecction
     for row in result:
         print("{}: {}".format(row.id, row.name))
