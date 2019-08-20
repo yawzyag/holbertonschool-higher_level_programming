58c26392ed3f761772bf787095364257ec1e09b5
yawzyag <yesid.yag@gmail.com>
2019-08-20 20:21:14 +0000

    [Feature] testing

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" adding comentario """
+
+def find_peak(list_of_integers):
+    """find a peak"""
+    if len(list_of_integers) == 0:
+        return None
+    if len(list_of_integers) == 1:
+        return 0
+    return list_of_integers[findPeakHelper(list_of_integers, 0, len(list_of_integers) - 1)]
+
+def findPeakHelper(num, start, end):
+    span = end - start
+    # span of 1 indicates 2 elements
+    if span == 1:
+        return start if num[start] > num[end] else end
+    mid = start + span // 2
+    if num[mid] < num[mid + 1]:
+        return findPeakHelper(num, mid, end)
+    return findPeakHelper(num, start, mid)
