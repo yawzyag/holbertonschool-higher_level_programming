9513c2257440a90f5dbf48db899425096599655c
yawzyag <yesid.yag@gmail.com>
2019-08-26 20:05:44 +0000

    [Feature] Python script that fetches https://intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this is a module for parsing url"""
+import requests
+
+if __name__ == "__main__":
+    response = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:$")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
