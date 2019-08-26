e9af8cdb94b045e139f703a899d55a6e97243ee8
yawzyag <yesid.yag@gmail.com>
2019-08-26 20:31:27 +0000

    [Fix]

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,8 +4,5 @@ from sys import argv
 import requests
 
 if __name__ == "__main__":
-    try:
-        r = requests.post(argv[1], data = {'key': argv[2]})
-        print(r.text)
-    except:
-        pass
+    r = requests.post(argv[1], data={'email': argv[2]})
+    print(r.text)
