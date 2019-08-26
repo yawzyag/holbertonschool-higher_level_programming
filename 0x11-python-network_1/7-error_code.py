bad924526065d1c65edbec32674abb0cf9edbc47
yawzyag <yesid.yag@gmail.com>
2019-08-26 20:48:58 +0000

    [Feature] Python script that takes in a URL, sends a request to the URL and displays the body of the response.

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""parse error codes"""
+import requests
+from sys import argv
+
+try:
+    url = argv[1]
+    r = requests.get(url)
+    r.raise_for_status()
+except Exception as a:
+    print('Error code: {}'.format(r.status_code))
+else:
+    print(r.text)
