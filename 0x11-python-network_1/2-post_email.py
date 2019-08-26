396478528b920251933b10908efafa07106c2f41
Yesid <31510821+yawzyag@users.noreply.github.com>
2019-08-26 13:40:52 -0500

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,13 +4,14 @@ from sys import argv
 import urllib.request
 import urllib.parse
 
-try:
-    email = argv[2]
-    data = urllib.parse.urlencode({'email': email})
-    data = data.encode('ascii')
-    url = argv[1]
-    req = request.Request(url, data)
-    with urllib.request.urlopen(req) as f:
-        print(f.read().decode('utf-8'))
-except:
-    pass
+if __name__ == "__main__":
+    try:
+        email = argv[2]
+        data = urllib.parse.urlencode({'email': email})
+        data = data.encode('ascii')
+        url = argv[1]
+        req = request.Request(url, data)
+        with urllib.request.urlopen(req) as f:
+            print(f.read().decode('utf-8'))
+    except:
+        pass
