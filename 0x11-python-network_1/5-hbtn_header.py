df01a07f9295ef85e072fe47fcad9c8cfaae708d
yawzyag <yesid.yag@gmail.com>
2019-08-26 23:21:02 +0000

    [Fix] try

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,8 +3,11 @@
 
 if __name__ == "__main__":
     import requests
-    from sys import argv
+    import sys
 
-    url = argv[1]
-    r = requests.get(url)
-    print(r.headers['X-Request-Id'])
+    try:
+        url = sys.argv[1]
+        r = requests.get(url)
+        print(r.headers['X-Request-Id'])
+    except:
+        pass
