06e848f5dcc44da91bba1ced408f7a7de1007e1d
yawzyag <yesid.yag@gmail.com>
2019-08-26 18:06:19 +0000

    [Feature] adding 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """parse error codes"""
-from urllib.request import Request, urlopen
+import urllib.request
 from urllib.error import HTTPError
 from sys import argv
 
 try:
-    req = Request(argv[1])
-    response = urlopen(req)
+    url = argv[1]
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print(html.decode('utf-8'))
 except HTTPError as e:
     print('Error code: {}'.format(e.code))
 except:
     pass
-else:
-    html = response.read()
-    print(html.decode('utf-8'))
