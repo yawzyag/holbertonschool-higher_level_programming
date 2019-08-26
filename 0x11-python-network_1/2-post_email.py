792939909fdb92819cc26f71d6bb405dcb99b5d3
yawzyag <yesid.yag@gmail.com>
2019-08-26 17:03:04 +0000

    [Feature] adding Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""holi"""
+from sys import argv
+import urllib.request
+import urllib.parse
+
+email = argv[2]
+data = urllib.parse.urlencode({'email': email})
+data = data.encode('ascii')
+url = argv[1]
+with urllib.request.urlopen(url, data) as f:
+    print(f.read().decode('utf-8'))
