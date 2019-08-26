f1f73b27ac55fdde46bd7023e7b1fd6cd2e69d1f
yawzyag <yesid.yag@gmail.com>
2019-08-26 20:10:36 +0000

    [Fix] pesos

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ import requests
 if __name__ == "__main__":
     response = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type:",type(response.text))
-    print("\t- content:",response.text)
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
