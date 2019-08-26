eadb81a7dac09619e8dfff63efd72a36d8f8be17
Yesid <31510821+yawzyag@users.noreply.github.com>
2019-08-26 13:46:52 -0500

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,13 +2,12 @@
 """holi"""
 from sys import argv
 import urllib.request
-import urllib.parse
 
 if __name__ == "__main__":
     email = argv[2]
     data = urllib.parse.urlencode({'email': email})
     data = data.encode('ascii')
     url = argv[1]
-    req = request.Request(url, data)
+    req = request.request.Request(url, data)
     with urllib.request.urlopen(req) as f:
         print(f.read().decode('utf-8'))
