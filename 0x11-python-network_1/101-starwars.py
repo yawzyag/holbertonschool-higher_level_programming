3ff110fe350f62fa8ba39d1795c2b64fe1601754
yawzyag <yesid.yag@gmail.com>
2019-08-27 03:31:18 +0000

    [Fix] adding get

---
+++ 0x11-python-network_1/101-starwars.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""parse error codes"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    payload = {'search': argv[1]}
+    url = "https://swapi.co/api/people"
+    req = requests.post(url, params=payload)
+    r = req.json()
+    num = r.get('count')
+
+    print("Number of results: {}".format(num))
+    for i in r.get('results'):
+        print(i.get('name'))
+    while r.get('next'):
+        r = requests.get(r.get('next')).json()
+        for i in r.get('results'):
+            print(i.get('name'))
