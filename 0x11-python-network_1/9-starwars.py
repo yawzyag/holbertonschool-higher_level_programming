713594616a4db645ac7dcdcf1a4e2b39bee44650
yawzyag <yesid.yag@gmail.com>
2019-08-27 00:48:03 +0000

    [Fix] params

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -5,8 +5,8 @@ from sys import argv
 
 if __name__ == "__main__":
     payload = {'search': argv[1]}
-    url = "https://swapi.co/api/people/"
-    req = requests.get(url, payload)
+    url = "https://swapi.co/api/people"
+    req = requests.post(url, params=payload)
     r = req.json()
     num = r.get('count')
 
