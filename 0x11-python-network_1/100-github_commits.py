1dc1528471e904f2604725975f02eeb41842c2fb
yawzyag <yesid.yag@gmail.com>
2019-08-27 01:47:17 +0000

    [Feature] adding 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""parse error codes"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[1], argv[2])
+    req = requests.get(url)
+    r = req.json()
+    for i in range(10):
+        print(
+            "{} {}".format(
+                r[i].get("sha"),
+                r[i].get("commit").get("author").get("name")))
