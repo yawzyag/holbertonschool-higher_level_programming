4c387571b72998c62cb18360a0c327594869b453
yawzyag <yesid.yag@gmail.com>
2019-09-23 21:07:55 +0000

    [Feature] creating 1 new file concatenate other 2

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const fileA = process.argv[2]; const fileB = process.argv[3]; const dest = process.argv[4];
+
+const fileTextA = fs.readFileSync(fileA);
+const fileTextB = fs.readFileSync(fileB);
+
+fs.writeFileSync(dest, fileTextA + fileTextB);
