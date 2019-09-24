ea7cf396140d037e08f620dacf7a4af0005daa0d
yawzyag <yesid.yag@gmail.com>
2019-09-24 17:21:02 +0000

    [fix] includes 18

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,7 @@ const request = require('request');
 const textA = `${process.argv[2]}`;
 
 request(textA, function (error, response, body) {
-  if (error) console.error('error:', error);
+  if (error) console.error(error);
   const json = JSON.parse(body);
   let count = 0;
   let title;
@@ -12,7 +12,7 @@ request(textA, function (error, response, body) {
 
   for (title of json.results) {
     for (char of title.characters) {
-      if (char === 'https://swapi.co/api/people/18/') count++;
+      if (char.includes('18')) count++;
     }
   }
   console.log(count);
