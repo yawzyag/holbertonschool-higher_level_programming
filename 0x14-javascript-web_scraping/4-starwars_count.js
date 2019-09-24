35fb5b8277a90d9421564e305056ec28f4562040
yawzyag <yesid.yag@gmail.com>
2019-09-24 17:16:42 +0000

    [Feature] adding 4 file

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+
+const textA = `${process.argv[2]}`;
+
+request(textA, function (error, response, body) {
+  if (error) console.error('error:', error);
+  const json = JSON.parse(body);
+  let count = 0;
+  let title;
+  let char;
+
+  for (title of json.results) {
+    for (char of title.characters) {
+      if (char === 'https://swapi.co/api/people/18/') count++;
+    }
+  }
+  console.log(count);
+});
